b43c29bb6c84519fe4df26d5d17badae9a1723d9
GBTC440002UR <GBTC440002UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-03-04 16:16:28 -0500

    MotorsCaretakersProject corrected 2

--- src/main/java/com/cristian/demo/MotorsCaretakersProjectApplication.java
+++ src/main/java/com/cristian/demo/MotorsCaretakersProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RegistrationFormApplication {
+public class MotorsCaretakersProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RegistrationFormApplication.class, args);
+        SpringApplication.run(MotorsCaretakersProjectApplication.class, args);
     }
 
 }
